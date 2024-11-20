087481e5594b7645b2487196c2016ead8fa02b8b
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2024-11-20 14:54:12 -0800

    added in notes file to hold the game notes from my  class notes

--- playerManagement.cpp
+++ playerManagement.cpp
@@ -20,3 +20,45 @@
 #include <array>
 
 using namespace std;
+
+// send get request to a web server
+int main (int argc, char* argv[]) {
+    // check that the command line has the right # of params
+    if (argc < 3){
+        cerr << "Error: Not enough parameters passed in. Usage: " << argv[0] << " <testNumber> <windowSize>\n";
+        return 1;
+    }
+
+    // params passed in through command line
+    int testNum = atoi(argv[1]);
+    int windowSize = atoi(argv[2]);
+
+    // make the socket structs and error check
+    // pass in serverIP/domain -- getaddrinfo does the DNS lookup for us, how nice!
+    struct addrinfo* servinfo = makeGetaddrinfo(HOST, PORT);
+
+    // make the socket
+    int clientSd = makeSocket(servinfo);
+
+    int resend;
+    // if sliding window print out the window size in results 
+    if (testNum == 3){
+        // get the start time of the test in usec
+        int start = startTimer();
+        // call the right test, tests call msg builds and they send the msg themselves
+        // tests also call readResponse() and processResponse()
+        resend = pickTest(testNum, clientSd, servinfo, windowSize);
+        // get the run time in usec
+        int runTime = stopTimer(start);
+        printWindowSizeTests(windowSize, resend, runTime);
+    } else {
+        resend = pickTest(testNum, clientSd, servinfo, DEFAULT_WINDOW_SIZE);
+        printResults(testNum, resend);
+    }
+
+    // call that handles error checks in other function
+    closeSocket(clientSd);
+    freeaddrinfo(servinfo);
+
+    return 0;
+}
