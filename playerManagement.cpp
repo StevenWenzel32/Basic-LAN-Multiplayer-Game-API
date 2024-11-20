a573931efc44a591cc7c63bdf3d9c02efa9bb0ad
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2024-11-20 14:50:41 -0800

    made files to hold the networking code

---
+++ playerManagement.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sys/types.h>    // socket, bind
+#include <sys/socket.h>   // socket, bind, listen, inet_ntoa
+#include <unistd.h>       // read, write, close
+#include <strings.h>      // bzero
+#include <netinet/tcp.h>  // SO_REUSEADDR
+#include <string.h>       // memset
+#include <errno.h>        // errno
+#include <fstream>        // ofstream for file creation
+#include <netdb.h>        // gethostbyname
+#include <sstream>        // for stringstream stuff
+#include <random>         // for random #
+#include <chrono>         // for steady_clock and timer stuff
+#include <queue>          // for queue, duh
+#include <cerrno>
+#include <cstring>        // for strerror
+#include <vector>
+#include <array>
+
+using namespace std;
