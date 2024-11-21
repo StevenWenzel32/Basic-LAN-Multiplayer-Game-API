170195eff90ca47098a69bd9d084ba440942a10a
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2024-11-20 16:57:41 -0800

    moved over some functions to be modified for the host, client and server from the sliding window project

--- knight-Host.cpp
+++ knight-Host.cpp
@@ -25,6 +25,114 @@
 
 using namespace std;
 
+// handle making the socket struct for listening -- makes a UDP socket
+// can later add in params to change the family and socktype and optional flags and port #
+struct addrinfo* makeGetaddrinfo(){
+    // for checking the return of getaddrinfo
+    int status;
+    // holds the info for the server address
+    struct addrinfo server_addr;
+    // points to the results that are in a linked list - is returned
+    struct addrinfo *servinfo; 
+    
+    // create the struct and address info
+    // make sure the struct is empty
+    memset(&server_addr, 0, sizeof(server_addr));
+    // doesn't matter if its ipv4 or ipv6
+    server_addr.ai_family = AF_UNSPEC;
+    // tcp stream sockets
+    server_addr.ai_socktype = SOCK_DGRAM;
+    // fill in my IP for me 
+    server_addr.ai_flags = AI_PASSIVE;
+
+    // getaddrinfo and error check in one -- doesn't need an IP/host because this is for listening
+    if ((status = getaddrinfo(NULL, PORT, &server_addr, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+    
+    return servinfo;
+}
+
+// make the listening socket, make it non-blocking, and do error checks, and return the Sd
+int makeListeningSocket(struct addrinfo* servinfo){
+    // open a stream-oriented socket with the internet address family
+    int serverSd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    // check if the socket call had an error
+    if (serverSd == -1) {
+        cerr << "error making the socket: serverSd - " << serverSd << endl;
+    }
+
+    // get the current flags
+    int flags = fcntl(serverSd, F_GETFL, 0);
+    // turn on the non-blocking flag
+    fcntl(serverSd, F_SETFL, flags | O_NONBLOCK); 
+
+    return serverSd;
+}
+
+// set the socket resue function to help free up unused sockets and ports
+void setSocketReuse(int serverSd){
+    // Enable socket reuse without waiting for the OS to recycle it
+    // set the so-reuseaddr option
+    const int on = 1;
+    int success = setsockopt(serverSd, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(int));
+    // check if the option call failed
+    if (success == -1) {
+        cerr << "Error setting the socket reuse option: serverSd - " << serverSd << endl;
+    }
+}
+
+// bind the socket
+void bindSocket(int serverSd, struct addrinfo* servinfo){
+    // Bind the socket to the port we passed into getaddrinfo
+    int binding = bind(serverSd, servinfo->ai_addr, servinfo->ai_addrlen);
+    // check if the bind had an error
+    if (binding == -1) {
+        cerr << "Error binding socket: serverSd - " << serverSd << " to port: " << PORT << endl;
+    }
+}
+
+// close the socket and check for errors
+void closeSocket(int sd){
+    int bye = close(sd);
+    if (bye == -1){
+        cerr << "Error closing socket" << endl;
+    }
+}
+
+// send a simple UDP msg as int[]
+void sendMsg(int sd, int message[], struct addrinfo *servinfo){
+    int bytes_sent = sendto(sd, message, BUFFER_SIZE, 0, servinfo->ai_addr, servinfo->ai_addrlen);
+    if (bytes_sent == -1){
+        cerr << "Problem with simple send" << endl;
+    }
+}
+
+// recieve and return the ackNum from server without waiting for a response - used for sliding window
+int readAckNoBlock(int clientSd, struct addrinfo* servinfo){
+    // "buffer" for reading in and returning the server ackNum
+    int ackNum = -1;
+    int nRead = 0;
+    nRead = recvfrom(clientSd, &ackNum, sizeof(int), MSG_DONTWAIT, servinfo->ai_addr, &(servinfo->ai_addrlen));
+        if (nRead == -1){
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                // do nothing simply nothing to read yet
+                ackNum = -1;
+            } else {
+                cerr << "Error reading from socket: clientSd = " << clientSd << endl;
+                ackNum = -2;
+            }
+        } else if (nRead == 0) {
+            cerr << "Server closed the connection" << endl;
+            ackNum = -3;
+        } else {
+// cout << "CLIENT Recieved via non blocking read - AckNum = " << ackNum << endl;
+            return ackNum;
+        }
+    return -4;
+}
+
 // manage the game world and the host player
 int main (int argc, char* argv[]) {
     // create/launch the level
--- server.cpp
+++ server.cpp
@@ -1,6 +1,36 @@
 // This is basically the lobby -- handles the player connections
 
+// go through and get rid of the includes that aren't needed
+#include <iostream>
+#include <sys/types.h>    // socket, bind
+#include <sys/socket.h>   // socket, bind, listen, inet_ntoa
+#include <netdb.h>        // gethostbyname
+#include <unistd.h>       // read, write, close, sleep
+#include <strings.h>      // bzero
+#include <netinet/tcp.h>  // SO_REUSEADDR
+#include <errno.h>        // errno
+#include <cerrno>
+#include <cstring>        // for strerror
+#include <string.h>       // for memset
+#include <filesystem>     // for exists
+#include <fstream>        // for file stuff
+#include <sstream>        // for stringstream stuff
+#include <vector>         // for vectors, duh
+#include <signal.h>       // for the shutdown signal
+#include <fcntl.h>        // for fcntl -- to set non-blocking
+#include <random>         // for random #
+
 using namespace std;
+
+// flag for shutting the server down -- ends all while loops - would probably be best to find a way to end for loops too
+volatile sig_atomic_t shutdown_flag = 0;
+
+// handles the shut down of the server
+void signalHandler(int signum) {
+    // cerr << "Server received shutdown signal: " << signum << ". Initiating clean shutdown..." << endl;
+    shutdown_flag = 1;
+}
+
 
 // manages the player: joining, hosting, and leaving
 int main (int argc, char* argv[]) {
