aa51c835cdb539c0835b3dd6ffb6f64f5a592490
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2024-11-21 16:14:36 -0800

    some include changes in basicNetworking

--- basicNetworking.hpp
+++ basicNetworking.hpp
@@ -2,33 +2,28 @@
 #ifndef BASIC_NETWORK_HPP
 #define BASIC_NETWORK_HPP
 
-// go through and get rid of the includes that aren't needed
+// unsure if being used
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>      // bzero
+#include <errno.h>        // errno
+#include <cerrno>
+#include <cstring>        // for strerror
+
+// being used
 #include <iostream>
 #include <sys/types.h>    // socket, bind
 #include <sys/socket.h>   // socket, bind, listen, inet_ntoa
-#include <unistd.h>       // read, write, close
-#include <strings.h>      // bzero
-#include <netinet/tcp.h>  // SO_REUSEADDR
-#include <string.h>       // memset
-#include <errno.h>        // errno
-#include <fstream>        // ofstream for file creation
 #include <netdb.h>        // gethostbyname
-#include <sstream>        // for stringstream stuff
-#include <random>         // for random #
-#include <queue>          // for queue, duh
-#include <cerrno>
-#include <cstring>        // for strerror
-#include <vector>
-#include <array>
-#include <unordered_map>
-#include <signal.h>       // for the shutdown signal
 #include <fcntl.h>        // for fcntl -- to set non-blocking
+#include <netinet/tcp.h>  // SO_REUSEADDR -- to set reuse
+#include <unistd.h>       // read, write, close
+#include <string.h>       // memset
 
 // port to use/ listen on
 #define PORT "2087"
-// # of connection requests for the server to listen to at a time, used in listen call
+// # of connection requests for the server to listen to at a time
+// based losely off of the class size
 #define BACKLOG 50
 
 using namespace std;
