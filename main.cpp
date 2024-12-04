05f058010802e96b16606c3aad4e88314f66a003
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2024-12-04 10:20:12 -0800

    added in listening socket for tcps msgs

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
 #include <csignal>
 
 // port to use to listen and send on broadcast
-#define PORT "2087"
+#define UDP_PORT "2087"
+#define TCP_PORT "2088"
 
 int main (int argc, char* argv[]) {
     // Set up signal handling for SIGINT and SIGTERM so that the client can stop listening nicely
@@ -17,7 +18,7 @@ int main (int argc, char* argv[]) {
     Player player;
 
     // setup the listening socket for broadcast msgs
-    struct addrinfo* clientinfo = makeAddrinfo("udp", PORT);
+    struct addrinfo* clientinfo = makeAddrinfo("udp", UDP_PORT);
     // make the socket
     player.broadSd = makeSocket(clientinfo);
     // set the option to resuse 
@@ -28,14 +29,21 @@ int main (int argc, char* argv[]) {
     bindSocket(player.broadSd, clientinfo);
     // free after being used -- will probably be moved again 
     freeaddrinfo(clientinfo);
-
-    // setup the socket for sending broadcast msgs
+    // setup the same socket as above for sending broadcast msgs
     // use ipv4
     player.broadcastAddr.sin_family = AF_INET;
     // convert the port to network byte order
-    player.broadcastAddr.sin_port = htons(atoi(PORT));
+    player.broadcastAddr.sin_port = htons(atoi(UDP_PORT));
     // set the boradcast addr
     player.broadcastAddr.sin_addr.s_addr = inet_addr("255.255.255.255");
+
+    // setup the TCP listening socket for connecting to other players
+    // get the info 
+    struct addrinfo* tcpinfo = makeAddrinfo("tcp", TCP_PORT);
+    player.tcpListenSd = makeSocket(tcpinfo);
+    setSocketReuse(player.tcpListenSd);
+    bindSocket(player.tcpListenSd, tcpinfo);
+    listening(player.tcpListenSd, 1);
 
     // Thread for listening for broadcast msgs -- always running until shutdown
     thread listenerThread(&Player::listenForMsgs, &player);
