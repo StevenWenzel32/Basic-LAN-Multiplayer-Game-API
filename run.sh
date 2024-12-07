2b26cacdd1bf621163f86b92a671d54a350c7c7d
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2024-12-06 17:41:45 -0800

    more files in run

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 # this is the demo file to run all test cases for the UDP server and client
-g++ -pthread main.cpp player.cpp basicNetworking.cpp TicTacToeGame.cpp -o tictactoe
-./tictactoe
+g++ -pthread main.cpp player.cpp basicNetworking.cpp TicTacToeGame.cpp globalFlags.cpp -o tictactoe
+# being run in an empty environment to try and resolve cin errors 
+env -i ./tictactoe
